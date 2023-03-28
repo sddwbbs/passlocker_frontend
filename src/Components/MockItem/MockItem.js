53aeb9993b9142c827ab079c3c9cff87a3b26919
Peter Golov <petya-golov@mail.ru>
2023-03-29 02:20:38 +0300

    Merges with master, conencted backend for get all notes, refresh tokens and log out

--- src/Components/MockItem/MockItem.js
+++ src/Components/MockItem/MockItem.js
@@ -1,15 +1,15 @@
-import { Card, Text } from '@nextui-org/react';
+import { Card, Text } from '@nextui-org/react'
 
 const MockItem = ({ text, onDelete }) => {
-  return (
-    <Card css={{ h: "200px", $$cardColor: 'white', mt: '50px'}}>
-      <Card.Body>
-        <Text h6 size={15} color="black" css={{ mt: 0 }}>
-          {text}
-        </Text>
-      </Card.Body>
-    </Card>
-  );
-};
+    return (
+        <Card css={{ h: '200px', $$cardColor: 'white', mt: '50px' }}>
+            <Card.Body>
+                <Text h6 size={15} color="black" css={{ mt: 0 }}>
+                    {text}
+                </Text>
+            </Card.Body>
+        </Card>
+    )
+}
 
-export default MockItem;
+export default MockItem
