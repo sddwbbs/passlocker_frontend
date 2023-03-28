94772e9f2d6c02a1216f293531aa69d34e849a77
Peter Golov <petya-golov@mail.ru>
2023-03-29 01:17:47 +0300

    Connect backend for get all passwords, refresh tokens and log out

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,11 +1,15 @@
 import './Modal.css'
 
-const Modal = props => {
-
+const Modal = (props) => {
     return (
-        <div className={`modal__wrapper ${props.isOpened ? "open" : "close"}`} style={{ ...props.style}}>
+        <div
+            className={`modal__wrapper ${props.isOpened ? 'open' : 'close'}`}
+            style={{ ...props.style }}
+        >
             <div className="modal__body">
-                <div className="modal__close" onClick={props.onModalClose}>×</div>
+                <div className="modal__close" onClick={props.onModalClose}>
+                    ×
+                </div>
                 <h2 className="modal__title">{props.title}</h2>
                 <hr />
                 {props.children}
