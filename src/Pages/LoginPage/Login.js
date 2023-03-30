2bd67fb647a390bdabe1ccef03d53e2d28b809b9
Ivan <d.from2016@yandex.ru>
2023-03-30 18:07:30 +0300

    refactoring

--- src/Pages/LoginPage/Login.js
+++ src/Pages/LoginPage/Login.js
@@ -5,55 +5,10 @@ import Navbar from '../../Components/Navbar'
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    // const [email, setEmail] = useState('')
-    // const [password, setPassword] = useState('')
-    // const [emailDirty, setEmailDirty] = useState(false)
-    // const [passwordDirty, setPasswordDirty] = useState(false)
-    // const [emailError, setEmailError] = useState('email не может быть пустым')
-    // const [passwordError, setPasswordError] = useState(
-    //     'пароль не может быть пустым'
-    // )
-
-    // const emailHandler = (e) => {
-    //     setEmail(e.target.value)
-    //     const re =
-    //         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    //     if (!re.test(String(e.target.value).toLowerCase())) {
-    //         setEmailError('некорректный email')
-    //         if (!e.target.value) {
-    //             setEmailError('email не может быть пустым')
-    //         }
-    //     } else {
-    //         setEmailError('')
-    //     }
-    // }
-
-    // const passwordHandler = (e) => {
-    //     setPassword(e.target.value)
-    //     if (e.target.value.length < 3) {
-    //         setPasswordError('пароль не должен быть короче трех символов')
-    //         if (!e.target.value) {
-    //             setPasswordError('пароль не может быть пустым')
-    //         }
-    //     } else {
-    //         setPasswordError('')
-    //     }
-    // }
 
     const handleSubmit = (event) => {
         event.preventDefault();
       };
-
-    // const blurHandler = (e) => {
-    //     switch (e.target.name) {
-    //         case 'email':
-    //             setEmailDirty(true)
-    //             break
-    //         case 'password':
-    //             setPasswordDirty(true)
-    //             break
-    //     }
-    // }
 
     return (
         <div className="no-scroll">
@@ -64,9 +19,7 @@ function Login() {
                 <div className="centered_log_page">
                     <input
                         className="input_email_log_page"
-                        // onChange={(e) => emailHandler(e)}
                         value={email}
-                        // onBlur={(e) => blurHandler(e)}
                         name="email"
                         type="email"
                         placeholder="email адрес"
@@ -77,9 +30,7 @@ function Login() {
                 <div className="centered_log_page">
                     <input
                         className="input_password_log_page"
-                        // onChange={(e) => passwordHandler(e)}
                         value={password}
-                        // onBlur={(e) => blurHandler(e)}
                         name="password"
                         type="password"
                         placeholder="пароль"
