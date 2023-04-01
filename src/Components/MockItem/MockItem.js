d04d428defc1383092cf3e61fb2cb819a9d7c605
Ivan <d.from2016@yandex.ru>
2023-04-02 00:46:10 +0300

    add button styles

--- src/Components/MockItem/MockItem.js
+++ src/Components/MockItem/MockItem.js
@@ -1,9 +1,10 @@
 import { Card, Text, Button } from '@nextui-org/react'
+import './MockItem.css'
 
 const MockItem = (props) => {
     return (
-        <Card css={{ h: '200px', $$cardColor: 'white', mt: '50px' }}>
-            <Card.Body>
+        <Card css={{ h: '200px', $$cardColor: 'white', mt: '50px', position: 'relative' }}>
+            <Card.Body className="card_body">
                 <Text h6 size={15} color="black" css={{ mt: 0 }}>
                     {props.text}
                 </Text>
@@ -12,10 +13,15 @@ const MockItem = (props) => {
                     color="white"
                     onPress={props.onDelete}
                     css={{
-                        width: 'min-content',
+                        position: 'absolute',
+                        top: '10px',
+                        right: '10px',
+                        fontSize: '20px',
+                        zIndex: 1,
+                        marginRight: '30px',
                     }}
                 >
-                    Удалить
+                    ×
                 </Button>
             </Card.Body>
         </Card>
