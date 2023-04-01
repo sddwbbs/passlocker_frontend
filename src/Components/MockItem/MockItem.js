803a125d0072be3fbb8aa03e6cb1cbe6c11d973a
Peter Golov <petya-golov@mail.ru>
2023-04-01 21:21:25 +0300

    adding delete passwords

--- src/Components/MockItem/MockItem.js
+++ src/Components/MockItem/MockItem.js
@@ -1,12 +1,26 @@
-import { Card, Text } from '@nextui-org/react'
+import { Card, Text, Button } from '@nextui-org/react'
 
-const MockItem = ({ text, onDelete }) => {
+const hello = () => {
+    console.log('hello')
+}
+
+const MockItem = (props) => {
     return (
         <Card css={{ h: '200px', $$cardColor: 'white', mt: '50px' }}>
             <Card.Body>
                 <Text h6 size={15} color="black" css={{ mt: 0 }}>
-                    {text}
+                    {props.text}
                 </Text>
+                <Button
+                    auto
+                    color="white"
+                    onPress={props.onDelete}
+                    css={{
+                        width: 'min-content',
+                    }}
+                >
+                    Удалить
+                </Button>
             </Card.Body>
         </Card>
     )
--- src/Pages/DashboardPage/Requests.js
+++ src/Pages/DashboardPage/Requests.js
@@ -29,7 +29,7 @@ export const getAllPasswords = (setPasswords) => {
             let data = error.response.data
 
             if (data.message === 'token is expired') {
-                refreshTokens().then(getAllPasswords)
+                refreshTokens().then(() => getAllPasswords(setPasswords))
             } else {
                 console.error(data.message)
             }
@@ -83,7 +83,7 @@ export const logOut = (navigate) => {
             Authorization: `Bearer ${localStorage.getItem('access_token')}`,
         },
     })
-        .then(() => {
+        .then((response) => {
             navigate('/')
 
             localStorage.removeItem('access_token')
@@ -95,5 +95,116 @@ export const logOut = (navigate) => {
             } else {
                 console.error(error)
             }
+        })
+}
+
+export const addPassword = (
+    passwords,
+    setPasswords,
+    setServiceName,
+    setLink,
+    setLogin,
+    setEmail,
+    setPassword,
+    serviceName,
+    link,
+    login,
+    email,
+    password,
+    modal,
+    setModal
+) => {
+    let url
+
+    if (process.env.REACT_APP_ENV === 'local') {
+        url = 'http://localhost:8080/api/p/add-password'
+    } else if (process.env.REACT_APP_ENV === 'prod') {
+        url = '/api/p/add-password'
+    }
+
+    axios({
+        method: 'post',
+        url: url,
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+            'Content-Type': 'application/json',
+        },
+        data: {
+            serviceName: serviceName,
+            link: link,
+            login: login,
+            email: email,
+            password: password,
+        },
+    })
+        .then((response) => {
+            let data = response.data
+            let newPassword = {
+                id: data.id,
+                serviceName: serviceName,
+                link: link,
+                login: login,
+                email: email,
+                password: password,
+            }
+
+            setPasswords((passwords) => [...passwords, newPassword]``)
+
+            setServiceName('')
+            setLink('')
+            setLogin('')
+            setEmail('')
+            setPassword('')
+
+            setModal({ ...modal, modal1: false })
+        })
+        .then(() => console.log(passwords))
+        .catch((error) => {
+            if (error.response === undefined) {
+                console.error('backend is disable')
+                return
+            }
+
+            let data = error.response.data
+
+            if (data.message === 'token is expired') {
+                refreshTokens().then(addPassword)
+            } else {
+                console.error(data.message)
+            }
+        })
+}
+
+export const deletePassword = (id, passwords, setPasswords) => {
+    let url
+
+    if (process.env.REACT_APP_ENV === 'local') {
+        url = 'http://localhost:8080/api/p/delete-password'
+    } else if (process.env.REACT_APP_ENV === 'prod') {
+        url = '/api/p/delete-password'
+    }
+
+    axios({
+        method: 'post',
+        url: url,
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+            'Content-Type': 'application/json',
+        },
+        data: {
+            password_id: id,
+        },
+    })
+        .then(() => {
+            let passwordIndex = passwords.map((e) => e.id).indexOf(id)
+            let filteredPasswords = passwords.filter(function (value, index) {
+                return index !== passwordIndex
+            })
+
+            setPasswords(filteredPasswords)
+        })
+        .then(() => console.log(passwords))
+        .catch((error) => {
+            console.error(error)
         })
 }
